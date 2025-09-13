674cfe12b38028cb16ada6178dddc39ae46c6862
Alseny <sacko.alseny87@gmail.com>
2025-09-13 03:04:59 +0200

    Ajout des fichier streamlit

--- Scripts/app_streamlit.py
+++ Scripts/app_streamlit.py
@@ -1,10 +1,9 @@
 import streamlit as st
 import joblib
 import numpy as np
+import os
 
 # Define feature names and descriptions
-# When you use this - replace these features with the cols from df.columns 
-# Do not include your target!
 feature_names = ['Mean Radius', 'Mean Texture', 'Mean Perimeter', 'Mean Area', 'Mean Smoothness',
                  'Mean Compactness', 'Mean Concavity', 'Mean Concave Points', 'Mean Symmetry', 'Mean Fractal Dimension',
                  'SE Radius', 'SE Texture', 'SE Perimeter', 'SE Area', 'SE Smoothness', 'SE Compactness', 'SE Concavity',
@@ -12,27 +11,27 @@ feature_names = ['Mean Radius', 'Mean Texture', 'Mean Perimeter', 'Mean Area', '
                  'Worst Area', 'Worst Smoothness', 'Worst Compactness', 'Worst Concavity', 'Worst Concave Points', 'Worst Symmetry',
                  'Worst Fractal Dimension']
 
+# Get the directory of this script
+current_dir = os.path.dirname(__file__)
+scaler_path = os.path.join(current_dir, 'scaler.pkl')
+model_path = os.path.join(current_dir, 'model.pkl')
+
 def main():
     st.title('Breast Cancer Prediction App by Dr. Lee')
     st.write("Enter the values for the features to get a prediction.")
 
     # Load sample data to pre-load default values
-    # When you use this on your model - comment out this line of code
-    sample_data = joblib.load('scaler.pkl').inverse_transform(np.array([[0]*30]))  # Placeholder for the actual sample data
+    sample_data = joblib.load(scaler_path).inverse_transform(np.array([[0]*30]))  # Placeholder for the actual sample data
 
     # Define input fields for user to enter feature values with proper labels
     features = []
     for i, feature in enumerate(feature_names):
         feature_value = st.number_input(f'{feature}', min_value=0.0, value=float(sample_data[0, i]))
-        # feature_value = st.number_input(f'{feature}', min_value=0.0, value=0.0)  # Use this line when you use your model
         features.append(feature_value)
 
-    # Handle cases where input features may need categorical encoding
+    # Handle cases where input features may need scaling
     def preprocess_input(features):
-        model = joblib.load('model.pkl')
-        scaler = joblib.load('scaler.pkl')
-
-        # Scale the features
+        scaler = joblib.load(scaler_path)
         features_scaled = scaler.transform(np.array(features).reshape(1, -1))
         return features_scaled
 
@@ -40,7 +39,7 @@ def main():
     if st.button('Predict'):
         try:
             features_scaled = preprocess_input(features)
-            model = joblib.load('model.pkl')
+            model = joblib.load(model_path)
             prediction = model.predict(features_scaled)
             probability = model.predict_proba(features_scaled)[0, 1]
 
